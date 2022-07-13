c9108246f2ec4445fe8e38de0dbdfc6971290582
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-07-13 20:56:19 +0300

    write test to check add-todolist action of reducer

--- src/components/todolists-reducer.test.ts
+++ src/components/todolists-reducer.test.ts
@@ -1,8 +1,8 @@
-import {todolistReducer} from './todolists-reducer';
-import {v1} from 'uuid';
+import {todolistReducer} from "./todolists-reducer";
+import {v1} from "uuid";
 import {TodoListsType} from "../App";
 
-test('correct todolist should be removed', () => {
+test("correct todolist should be removed", () => {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
@@ -11,8 +11,26 @@ test('correct todolist should be removed', () => {
         {id: todolistId2, title: "What to buy", filter: "all"}
     ]
 
-    const endState = todolistReducer(startState, { type: 'REMOVE-TODOLIST', id: todolistId1})
+    const endState = todolistReducer(startState, {type: "REMOVE-TODOLIST", id: todolistId1})
 
     expect(endState.length).toBe(1);
     expect(endState[0].id).toBe(todolistId2);
 });
+
+test("correct todolist should be added", () => {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    let newTodolistTitle = "New Todolist";
+
+    const startState: Array<TodoListsType> = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+
+    const endState = todolistReducer(startState, {type: "ADD-TODOLIST", title: newTodolistTitle})
+
+    expect(endState.length).toBe(3);
+    expect(endState[2].title).toBe(newTodolistTitle);
+});
+
--- src/components/todolists-reducer.ts
+++ src/components/todolists-reducer.ts
@@ -1,4 +1,5 @@
 import {TodoListsType} from "../App";
+import {v1} from "uuid";
 
 
 type ActionType = {
@@ -11,7 +12,13 @@ export const todolistReducer = (state: Array<TodoListsType>, action: ActionType)
     switch (action.type) {
         case "REMOVE-TODOLIST":
             return state.filter(el => el.id !== action.id)
+        case "ADD-TODOLIST":
+            let newTodolist = {id: v1(), title: action.title, filter: "all"}
+            return [...state, newTodolist]
         default:
             throw new Error("I don't understand this type")
     }
 }
+
+
+
