9f31ac80479c543ebd13318d16a46d8f45d2d346
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-07-13 19:27:40 +0300

    testing test-reducers

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -17,4 +17,15 @@ test('user reducer should increment only childrenCount', () => {
     expect(endState.childrenCount).toBe(3)
 });
 
+test('user reducer should change name of user', () => {
+    const startState = { name: 'Dimych', age: 20, childrenCount: 2 };
+
+    const newName = 'Viktor';
+
+    const endState = userReducer(startState, { type: 'CHANGE-NAME', newName: newName})
+
+    expect(endState.name).toBe(newName);
+});
+
+
 
--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -16,6 +16,9 @@ export const userReducer = (state: StateType, action: ActionType) => {
             return state;
         case 'INCREMENT-CHILDREN-COUNT':
             return {...state, childrenCount: state.childrenCount + 1};
+
+        case 'CHANGE-NAME':
+            return {...state, name: action.newName}
         default:
             throw new Error("I don't understand this type")
     }
