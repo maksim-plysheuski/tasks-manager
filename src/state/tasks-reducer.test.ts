bc1879a07e81aa12da7b5f6b9037daf0571fa2f1
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-10-04 00:03:15 +0300

    writing test for tasksReducer "CHANGE-STATUS" case using TDD development's method

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,6 +1,6 @@
 import {TasksStateType} from "../App"
 import {v1} from "uuid";
-import {addTaskAC, tasksReducer} from "./tasks-reducer"
+import {addTaskAC, removeTaskAC, tasksReducer} from "./tasks-reducer"
 
 
 test("task should be added in correct todolist", () => {
@@ -30,7 +30,6 @@ test("task should be added in correct todolist", () => {
 test("correct task should be removed", () => {
     let todolistID1 = v1()
     let todolistID2 = v1()
-    let newTitle: string = "React"
 
     let startState: TasksStateType = {
         [todolistID1]: [
@@ -46,7 +45,14 @@ test("correct task should be removed", () => {
 
     let endState: TasksStateType = tasksReducer(startState, removeTaskAC(todolistID2, "3"))
 
-    expect(endState).toBe({
+    expect(endState[todolistID2].length).toBe(2)
+})
+
+test("correct task should change status", () => {
+    let todolistID1 = v1()
+    let todolistID2 = v1()
+
+    let startState: TasksStateType = {
         [todolistID1]: [
             {id: "1", title: "HTML&CSS", isDone: true},
             {id: "2", title: "JS", isDone: true},
@@ -55,5 +61,9 @@ test("correct task should be removed", () => {
             {id: "1", title: "Milk", isDone: false},
             {id: "2", title: "Butter", isDone: true},
         ]
-    })
+    };
+
+    let endState: TasksStateType = tasksReducer(startState, changeTaskStatusAC(todolistID2, "1", true))
+
+    expect(endState[todolistID2]['1'].isDone).toBe(true)
 })
--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,13 +1,21 @@
 import {TasksStateType} from "../App";
 import {v1} from "uuid";
 
-type addTaskACType = ReturnType<typeof addTaskAC>
 
-export function tasksReducer(state: TasksStateType, action: addTaskACType) {
+type actionTypes = addTaskACType | removeTaskAC
+
+
+type addTaskACType = ReturnType<typeof addTaskAC>
+type removeTaskAC = ReturnType<typeof removeTaskAC>
+
+export function tasksReducer(state: TasksStateType, action: actionTypes) {
     switch (action.type) {
         case "ADD-TASK": {
             let newTask = {id: v1(), title: action.title, isDone: false}
             return {...state, [action.id]: [newTask, ...state[action.id]]}
+        }
+        case "REMOVE-TASK": {
+            return {...state, [action.todolistID]: state[action.todolistID].filter(t => t.id !== action.taskID)}
         }
         default: {
             return state
@@ -18,6 +26,6 @@ export function tasksReducer(state: TasksStateType, action: addTaskACType) {
 
 export const addTaskAC = (todolistID: string, newTitle: string) => ({type: "ADD-TASK", id: todolistID, title: newTitle} as const)
 
-export const removeTaskAC = (todolistID: string, taskID: string) => ({type: "REMOVE-TASK", id: taskID})
+export const removeTaskAC = (todolistID: string, taskID: string) => ({type: "REMOVE-TASK",todolistID: todolistID, taskID: taskID} as const)
 
 
