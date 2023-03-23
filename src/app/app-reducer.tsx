6b14fd3de51ffaab9f6ea490168d74d63fadb048
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-03-23 22:58:08 +0400

    add action and case in appReducer to handle error

--- src/app/app-reducer.tsx
+++ src/app/app-reducer.tsx
@@ -2,6 +2,7 @@ export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
     status: 'idle' as RequestStatusType,
+    error: null as null | string
 }
 
 type InitialStateType = typeof initialState
@@ -10,12 +11,16 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
             return state
     }
 }
 
 export const setStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const setErrorAC = (error: null | string) => ({type: 'APP/SET-ERROR', error} as const)
 
 export type SetStatusType = ReturnType<typeof setStatusAC>
-type ActionsType = SetStatusType
+export type setErrorType = ReturnType<typeof setErrorAC>
+type ActionsType = SetStatusType | setErrorType
