0e08e6a8f9aadbc10a31980fafdd3064007bdada
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-03-23 22:39:20 +0400

    add ErrorSnackbar component to handle errors

---
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export const CustomizedSnackbars = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+    return (
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    This is a success message!
+                </Alert>
+            </Snackbar>
+    );
+}
