e7bbb877e6c0f403027efe41dcae0fc508b1257d
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-04-28 22:36:33 +0300

    feet: unit tests for auth reducer

---
+++ src/features/auth/model/tests/auth-slice.test.ts
@@ -0,0 +1,37 @@
+import { authReducer, authThunks } from "features/auth/model/auth-reducer";
+
+
+let startState = {} as ReturnType<typeof authReducer>;
+
+beforeEach(() => {
+  startState = {
+    isLoggedIn: false,
+    userProfile: {
+      id: 123,
+      email: "alex@gmail.com",
+      login: "alex"
+    }
+  };
+});
+
+
+describe("Auth Reducer", () => {
+  it("should set user profile when initializeApp.fulfilled is dispatched", () => {
+    const profile = { id: 789, email: "alex@tests.com", login: "alex007" };
+
+    const endState = authReducer(startState, authThunks.initializeApp.fulfilled({
+      profile,
+      isLoggedIn: true
+    }, "requestId"));
+
+    expect(endState.userProfile).toEqual(profile);
+    expect(endState.isLoggedIn).toBe(true);
+  });
+
+
+  it("should set isLoggedIn to false when logout.fulfilled is dispatched", () => {
+    const endstate = authReducer(startState, authThunks.logout.fulfilled({ isLoggedIn: false }, "requestId"));
+
+    expect(endstate.isLoggedIn).toBe(false);
+  });
+});
